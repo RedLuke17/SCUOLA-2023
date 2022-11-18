77d3469f86e902a12dc0ed05f319d3353b4090d3
RedLuke17 <lukeishere17@gmail.com>
2022-11-18 18:42:21 +0100

    esercizio voti

---
+++ TDP/EserciziDaBeginners/voto/static/js/index.js
@@ -0,0 +1,22 @@
+function votoCalc(voto){
+
+    if( voto < 6.0 ){
+        return "insufficiente"
+    }
+    else if( voto <= 6.9 ){
+        return "sufficiente"
+    }
+    else if( voto <= 7.9 ){
+        return "il voto è discreto"
+    }
+    else{
+        return "il voto è buono"
+    }
+}
+
+let inputVoto = document.querySelector("#voto1")
+inputVoto.addEventListener("submit" , function(e){
+    e.preventDefault()
+    let voti = document.querySelector("#voto").value;
+    let output = document.querySelector("#print").innerHTML = votoCalc(voti);
+})
