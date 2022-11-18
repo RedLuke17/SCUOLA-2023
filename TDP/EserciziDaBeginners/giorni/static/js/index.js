14366b42f9c40281b508a95dbd25ed850e88ca58
RedLuke17 <lukeishere17@gmail.com>
2022-11-18 17:42:12 +0100

    primo es

---
+++ TDP/EserciziDaBeginners/giorni/static/js/index.js
@@ -0,0 +1,12 @@
+function giorni(){
+    let month = document.querySelector("#month").value;
+    let year = document.querySelector("#year").value;
+
+
+    if(month > 0){
+        document.querySelector("#print").innerHTML = (new Date(year,month,0).getDate())
+    }else{
+        document.querySelector("#print").innerHTML = "non hai inserito nessun valore"
+    }
+    
+}
