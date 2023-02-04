6508a93692557a0eb925b7430aae61eecf3fd3ba
Luca Rossi <lukeishere17@gmail.com>
2023-02-04 10:43:18 +0100

    update

--- TDP/Tris/static/js/index.js
+++ TDP/Tris/static/js/index.js
@@ -2,32 +2,16 @@ let player = "X";
 let squares = document.querySelectorAll(".square");
 let restart = document.querySelector(".restart-btn")
 let gameOver = false;
-const winningCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-const result = document.getElementById('result');
-let restartBtn = document.getElementById('restart-btn');
-const scoreX = document.getElementById('score-x');
-const scoreO = document.getElementById('score-o');
-let xScore = 0;
-let oScore = 0;
-let scoreX;
+let scoreX = 0;
 let scoreO = 0;
 
 
 for (let i = 0; i < squares.length; i++)
 {
     squares[i].addEventListener("click", function(e) {
-        if(gameOver == false)
+        if(gameOver === false)
         {
-            if(squares[i].textContent == "")
+            if(squares[i].textContent === "")
             {
                 squares[i].innerHTML = player;
                 switchPlayer();
@@ -41,7 +25,7 @@ function switchPlayer()
     if (player === "X")
     {
         player = "O";
-        document.querySelector(".scoreO").innerHTML = "O:"+scoreO++;
+        document.querySelector(".scoreO").innerHTML = scoreO++;
     } else
     {
         player = "X";
