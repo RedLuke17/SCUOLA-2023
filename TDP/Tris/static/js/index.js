98e23d89275cb48271d8cf61c2c3eb5d81ecfc27
RedLuke17 <lukeishere17@gmail.com>
2023-02-08 22:45:18 +0100

    update

--- TDP/Tris/static/js/index.js
+++ TDP/Tris/static/js/index.js
@@ -4,6 +4,16 @@ let restart = document.querySelector(".restart-btn")
 let gameOver = false;
 let scoreX = 0;
 let scoreO = 0;
+let winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
 
 for (let i = 0; i < squares.length; i++)
@@ -11,10 +21,28 @@ for (let i = 0; i < squares.length; i++)
     squares[i].addEventListener("click", function(e) {
         if(gameOver === false)
         {
-            if(squares[i].textContent === "")
+            if(squares[i].innerHTML === "")
             {
                 squares[i].innerHTML = player;
                 switchPlayer();
+
+                for (let i = 0; i < winningCombinations.length; i++) {
+                    let a = winningCombinations[i][0];
+                    let b = winningCombinations[i][1];
+                    let c = winningCombinations[i][2];
+                    if (squares[a].innerHTML === player && squares[b].innerHTML === player && squares[c].innerHTML === player) {
+                        gameOver = true;
+                        if(player === "X")
+                        {
+                            scoreX++
+                            document.querySelector(".scoreX").innerHTML = scoreX;
+                        } else
+                        {
+                            scoreO++
+                            document.querySelector(".scoreO").innerHTML = scoreO;
+                        }
+                    }
+                }
             }
         }
     });
@@ -25,7 +53,6 @@ function switchPlayer()
     if (player === "X")
     {
         player = "O";
-        document.querySelector(".scoreO").innerHTML = scoreO++;
     } else
     {
         player = "X";
