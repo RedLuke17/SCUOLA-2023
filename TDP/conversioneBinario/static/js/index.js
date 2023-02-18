c130fd5e15d83cae7884fb4521c216c961e1938b
RedLuke17 <lukeishere17@gmail.com>
2023-02-18 19:21:55 +0100

    conversione

---
+++ TDP/conversioneBinario/static/js/index.js
@@ -0,0 +1,10 @@
+function conversione(giglo) {
+    if(giglo === 0) {
+        return ""
+    } else {
+        let barilla = giglo%2
+        return conversione((giglo-barilla)/2) + barilla
+    }
+}
+
+//barilla è il resto
