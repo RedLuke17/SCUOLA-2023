38ec90365a7f1a4dda58961380e718082f66bd95
RedLuke17 <lukeishere17@gmail.com>
2023-05-07 14:38:18 +0200

    update

--- TDP/ChuckNorris/static/js/index.js
+++ TDP/ChuckNorris/static/js/index.js
@@ -1,6 +1,8 @@
 let categories = ["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"]
 let joke = document.querySelector(".joke")
-let copyBTN = document.querySelector(".copyBox")
+let copyBox = document.querySelector(".copyBox")
+let copyBTN = document.querySelector(".copyBTN")
+let link = document.querySelector(".link")
 
 for(let i = 0;i<categories.length;i++){
     let option = document.createElement("option")
@@ -20,12 +22,23 @@ document.querySelector("#mainForm").addEventListener("submit", function(e){
     if(currentCategory != ""){
         url += `?category=${currentCategory}`
         fetch(url).then(response => response.json())
-        .then(data => { joke.innerHTML = `${data.value}`})
+        .then(data => { joke.innerHTML = `${data.value}`
+        link.href = data.url})
+    } else {
+        fetch(url).then(response => response.json())
+        .then(data => { joke.innerHTML = `${data.value}`
+        link.href = data.url})
     }
 
     console.log(url)
 })
 
-copyBTN.addEventListener("click", () => {
+copyBox.addEventListener("click", () => {
     navigator.clipboard.writeText(joke.innerText)
+})
+
+copyBTN.addEventListener("click", () => {
+    copyBTN.innerHTML = "Battuta copiata"
+    setTimeout(function(){ copyBTN.innerHTML = "copia" },1500)
+    
 })
