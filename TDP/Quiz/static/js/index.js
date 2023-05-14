02ae3778b43ec5520113369262068fccfa62e15a
RedLuke17 <lukeishere17@gmail.com>
2023-05-14 16:01:48 +0200

    update

--- TDP/Quiz/static/js/index.js
+++ TDP/Quiz/static/js/index.js
@@ -1,21 +1,43 @@
 let form = document.querySelector(".mainForm")
+let container = document.querySelector(".container")
 let values = []
-for (let i = 1; i <= 3; i++) {
-    let div = document.createElement("div")
-    div.id = "question-"+i
-    form.appendChild(div)
+let questions = []
+let answers = ["la cozza come la preferisci?", "sei un giglo?"]
+let i = -1
 
-    let button = document.createElement("input")
-    button.type = "radio"
-    button.id = "answer-1"
-    button.name = "answer-1"
-    button.value = randomValue(10,5,-10)
-    div.appendChild(button)
 
-    let label = document.createElement("label")
-    label.innerHTML = randomText(values)
-    div.appendChild(label)
+
+let domande = {
+    questions
 }
+
+console.log(domande)
+
+    for (let i = 0; i < 3; i++) {
+        
+
+        let div = document.createElement("div")
+        div.id = "question-"+(i+1)
+        form.appendChild(div)
+    
+        let button = document.createElement("input")
+        button.type = "radio"
+        button.id = "answer-1"
+        button.name = "answer-1"
+        if (i < 1){
+            randomValue(10,5,-10)
+        }
+        button.value = values[i]
+        div.appendChild(button)
+    
+        let label = document.createElement("label")
+        randomQuestion(values)
+        label.innerHTML = questions[i]
+        div.appendChild(label)
+    }
+
+
+
 
 
 
@@ -25,19 +47,21 @@ function randomValue(min,med,max) {
     return values.sort(() => Math.random() -0.5)
 }
 
-function randomText(values) {
-    for (let i = 0; i < values.length; i++) {
-        if (values[i] == 10) {
-            return  "cruda"
-        }
-        if (values[i] == 5) {
-            return "cotta"
-        }
-        if (values[i] == -10) {
-            return "GIGLICAA"
-        }
+function randomQuestion(values) {
+    i++
+    if (values[i] == 10) {
+        questions[i] = "cruda"
+        return randomQuestion(values)
     }
-
+    if (values[i] == 5) {
+        questions[i] = "cotta"
+        return randomQuestion(values)
+    }
+    if (values[i] == -10) {
+        questions[i] = "al ragù"
+        return randomQuestion(values)
+    }
 }
 
-console.log(randomValue(10, 5, -10))
+console.log(i)
+
