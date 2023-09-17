c2a0125595e5e055b87d17075e99d78e13241148
RedLuke17 <lukeishere17@gmail.com>
2023-09-17 14:36:49 +0200

    react setup

--- TDP/Weather/static/js/index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".formwrapper").addEventListener("submit", function(event) {
-    event.preventDefault()
-    let latitudine = document.querySelector("#lat").value;
-    let longitudine = document.querySelector("#lng").value;
-
-    console.log(latitudine + longitudine)
-
-    let url;
-})
